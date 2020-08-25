70ed74dcbb3e68f1360430559a25a1027461c1cc
Noor Mahammad Chervu <noormahammad@users.noreply.github.com>
2020-08-25 07:52:02 -0400

    Data binding: Views usage

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -3,32 +3,42 @@ package com.example.aboutme
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.ScriptGroup
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val doneButton:Button = findViewById<Button>(R.id.done_button)
-        doneButton.setOnClickListener {
+        //val doneButton:Button = findViewById<Button>(R.id.done_button)
+
+        binding.doneButton.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view: View) {
-        val nickNameEditText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+        //val nickNameEditText = findViewById<EditText>(R.id.nickname_edit)
+        //val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
-        nickNameTextView.text = nickNameEditText.text
-        nickNameTextView.visibility = View.VISIBLE
-        nickNameEditText.visibility = View.GONE
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            invalidateAll()
+            nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+        }
 
-        view.visibility = View.GONE
 
     }
 }
