5677845ea5e9804e51861a88a572ec42a917e406
Noor Mahammad Chervu <noormahammad@users.noreply.github.com>
2020-08-24 21:14:31 -0400

    Adding Done button to add a nick name

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.aboutme
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val doneButton:Button = findViewById<Button>(R.id.done_button)
+        doneButton.setOnClickListener {
+            addNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View) {
+        val nickNameEditText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameTextView.text = nickNameEditText.text
+        nickNameTextView.visibility = View.VISIBLE
+        nickNameEditText.visibility = View.GONE
+
+        view.visibility = View.GONE
+
     }
 }
